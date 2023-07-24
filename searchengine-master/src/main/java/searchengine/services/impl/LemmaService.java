4b833c23fe6e75b343792b3ed472f3867aec8c2d
Yury Rakala <yuryrakala@gmail.com>
2023-07-24 22:33:43 +0300

    Working on service, continue

---
+++ searchengine-master/src/main/java/searchengine/services/impl/LemmaService.java
@@ -0,0 +1,74 @@
+package searchengine.services.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.lucene.morphology.LuceneMorphology;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+@Data
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class LemmaService {
+
+    private LuceneMorphology luceneMorphology;
+    private static final String WORD_TYPE_REGEX = "\\W\\w&&[^а-яА-Я\\s]";
+    private static final String[] particlesNames = new String[]{"МЕЖД", "ПРЕДЛ", "СОЮЗ"};
+
+    public Map<String, Integer> collectLemmas(String text) {
+        String[] words = arrayContainsRussianWords(text);
+        HashMap<String, Integer> lemmas = new HashMap<>();
+
+        for (String word : words) {
+            if (word.isBlank()) {
+                continue;
+            }
+
+            List<String> wordBaseForms = luceneMorphology.getMorphInfo(word);
+            if (anyWordBaseBelongToParticle(wordBaseForms)) {
+                continue;
+            }
+
+            List<String> normalForms = luceneMorphology.getNormalForms(word);
+            if (normalForms.isEmpty()) {
+                continue;
+            }
+
+            String normalWord = normalForms.get(0);
+
+            if (lemmas.containsKey(normalWord)) {
+                lemmas.put(normalWord, lemmas.get(normalWord) + 1);
+            } else {
+                lemmas.put(normalWord, 1);
+            }
+        }
+
+        return lemmas;
+    }
+
+    private String[] arrayContainsRussianWords(String text) {
+        return text.toLowerCase(Locale.ROOT)
+                .replaceAll("([^а-я\\s])", " ")
+                .trim()
+                .split("\\s+");
+    }
+
+    private boolean anyWordBaseBelongToParticle(List<String> wordBaseForms) {
+        return wordBaseForms.stream().anyMatch(this::hasParticleProperty);
+    }
+
+    private boolean hasParticleProperty(String wordBase) {
+        for (String property : particlesNames) {
+            if (wordBase.toUpperCase().contains(property)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
